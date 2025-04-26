8e1a1d906e58f39222e0137bd1323ac38a3adcde
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-26 21:45:12 +0330

    add logic to hide dice when game ends

--- script.js
+++ script.js
@@ -62,9 +62,10 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // check if player's score is >= 100
-    if (scores[activePlayer] >= 50) {
+    if (scores[activePlayer] >= 100) {
       // finish game
       playing = false;
+      diceElem.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
