a35c0c40ec8a7e815d4ba17a2bb389fad16c07d1
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-26 19:11:29 +0330

    select score and dice elements, set starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// select elements
+const score0Elem = document.getElementById('score--0');
+const score1Elem = document.getElementById('score--1');
+const diceElem = document.querySelector('.dice');
+
+// starting conditions
+score0Elem.textContent = 0;
+score1Elem.textContent = 0;
+diceElem.classList.add('hidden');
